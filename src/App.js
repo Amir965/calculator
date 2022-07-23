808e8c46f4582b6414a0bc1ce52dfa8d3419a8a7
Amir965 <sohailaamir965@gmail.com>
2022-07-23 19:39:55 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,87 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React,{useState} from "react"
 function App() {
+  const [result, setResult] = useState("")
+  const handleClick=(e) => {
+    setResult(result.concat(e.target.name))
+  }
+
+  const clear = () => {
+    setResult("")
+  }
+  const backspace = () => {
+    setResult(result.slice(0,-1))
+  }
+  const calculate = () => {
+    try {
+      setResult(eval(result).toString())
+    } catch (error) {
+      setResult("Error")
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <form>
+          <input type="text" value={result} />
+        </form>
+        <div className="keypad">
+          <button className="highlighing" onClick={clear} id="clear">
+            clear
+          </button>
+          <button className="highlighing" onClick={backspace} id="backspace">
+            c
+          </button>
+          <button className="highlighing" onClick={handleClick} name="/">
+            &divide;
+          </button>
+          <button onClick={handleClick} name="7">
+            7
+          </button>
+          <button onClick={handleClick} name="8">
+            8
+          </button>
+          <button onClick={handleClick} name="9">
+            9
+          </button>
+          <button className="highlighing" onClick={handleClick} name="*">
+            &times;
+          </button>
+          <button onClick={handleClick} name="4">
+            4
+          </button>
+          <button onClick={handleClick} name="5">
+            5
+          </button>
+          <button onClick={handleClick} name="6">
+            6
+          </button>
+          <button className="highlighing" onClick={handleClick} name="-">
+            &ndash;
+          </button>
+          <button onClick={handleClick} name="1">
+            1
+          </button>
+          <button onClick={handleClick} name="2">
+            2
+          </button>
+          <button onClick={handleClick} name="3">
+            3
+          </button>
+          <button className="highlighing" onClick={handleClick} name="+">
+            +
+          </button>
+          <button onClick={handleClick} name="0">
+            0
+          </button>
+          <button onClick={handleClick} name=".">
+            .
+          </button>
+          <button className="highlighing" onClick={calculate} id="result">
+            =
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
